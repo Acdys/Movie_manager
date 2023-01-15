d3545bdbf81e31fff405e05a2ac54d83bfc892f5
Lisa <velichkoeliza@gmail.com>
2023-01-15 17:18:49 +0300

    Initial commit

--- src/main/java/ru/netology/MovieManager.java
+++ src/main/java/ru/netology/MovieManager.java
@@ -2,22 +2,29 @@ package ru.netology;
 
 public class MovieManager {
     private String[] movies = new String[0];
+    private final int limit;
 
+    public MovieManager() {
+        this.limit = 10;
+    }
 
-    public void addMovie(String movie){
+    public MovieManager(int limit) {
+        this.limit = limit;
+    }
+
+    public void addMovie(String movie) {
         String[] tmp = new String[movies.length + 1];
         System.arraycopy(movies, 0, tmp, 0, movies.length);
         tmp[tmp.length - 1] = movie;
         movies = tmp;
-
     }
 
-    public String[] findAll(){
+    public String[] findAll() {
         return movies;
     }
 
-    public String[] findLast(){
-        int resultLength = Math.min(movies.length, 10);
+    public String[] findLast() {
+        int resultLength = Math.min(movies.length, limit);
         String[] tmp = new String[resultLength];
         for (int i = 0; i < tmp.length; i++) {
             tmp[i] = movies[movies.length - 1 - i];
--- src/test/java/ru/netology/MovieManagerTest.java
+++ src/test/java/ru/netology/MovieManagerTest.java
@@ -5,6 +5,50 @@ import org.junit.jupiter.api.Test;
 
 public class MovieManagerTest {
 
+    @Test
+    void shouldMovieManagerMoreLimit (){
+        String movie1 = "Bloodshot";
+        String movie2 = "Onward";
+        String movie3 = "Hotel Belgrade";
+        String movie4 = "Gentlemen";
+        String movie5 = "The Invisible Man";
+        String movie6 = "Trolls World Tour";
+        String movie7 = "Number one";
+        String movie8 = "White noise";
+        String movie9 = "Call me";
+        String movie10 = "Soul";
+        String movie11 = "Dark shadow";
+
+        MovieManager manager = new MovieManager(11);
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+        manager.addMovie(movie4);
+        manager.addMovie(movie5);
+        manager.addMovie(movie6);
+        manager.addMovie(movie7);
+        manager.addMovie(movie8);
+        manager.addMovie(movie9);
+        manager.addMovie(movie10);
+        manager.addMovie(movie11);
+
+        String[] actual = manager.findAll();
+        String[] expected = {movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10, movie11};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldMovieManagerMinLimit (){
+        String movie1 = "Bloodshot";
+
+        MovieManager manager = new MovieManager(1);
+        manager.addMovie(movie1);
+
+        String[] actual = manager.findAll();
+        String[] expected = {movie1};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
    @Test
     void shouldFindAllMovie (){
         String movie1 = "Bloodshot";
@@ -50,6 +94,71 @@ public class MovieManagerTest {
 
         String[] actual = manager.findLast();
         String[] expected = {movie7, movie6, movie5, movie4, movie3, movie2, movie1};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldFindLastThreeMovie () {
+        String movie5 = "The Invisible Man";
+        String movie6 = "Trolls World Tour";
+        String movie7 = "Number one";
+
+        MovieManager manager = new MovieManager();
+        manager.addMovie(movie5);
+        manager.addMovie(movie6);
+        manager.addMovie(movie7);
+
+        String[] actual = manager.findLast();
+        String[] expected = {movie7, movie6, movie5};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldFindFiveMovie (){
+        String movie1 = "Bloodshot";
+        String movie2 = "Onward";
+        String movie3 = "Hotel Belgrade";
+        String movie4 = "Gentlemen";
+        String movie5 = "The Invisible Man";
+
+
+        MovieManager manager = new MovieManager();
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+        manager.addMovie(movie4);
+        manager.addMovie(movie5);
+
+
+        String[] actual = manager.findLast();
+        String[] expected = {movie5, movie4, movie3, movie2, movie1};
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldFindAllAddMovie (){
+        String movie1 = "Bloodshot";
+        String movie2 = "Onward";
+        String movie3 = "Hotel Belgrade";
+        String movie4 = "Gentlemen";
+        String movie5 = "The Invisible Man";
+        String movie6 = "Trolls World Tour";
+        String movie7 = "Number one";
+        String movie8 = "White noise";
+
+
+        MovieManager manager = new MovieManager();
+        manager.addMovie(movie1);
+        manager.addMovie(movie2);
+        manager.addMovie(movie3);
+        manager.addMovie(movie4);
+        manager.addMovie(movie5);
+        manager.addMovie(movie6);
+        manager.addMovie(movie7);
+        manager.addMovie(movie8);
+
+        String[] actual = manager.findAll();
+        String[] expected = {movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8};
 
         Assertions.assertArrayEquals(expected, actual);
     }
